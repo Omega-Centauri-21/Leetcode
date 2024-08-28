24ec2c81d8bacfdcbb37a65b5aadf61115dd9795
Omega-Centauri-21 <69707370+Omega-Centauri-21@users.noreply.github.com>
2024-08-28 23:55:56 +0530

    Create 485_max-consecutive-ones.py

---
+++ arrays/485_max-consecutive-ones.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        res, count = 0, 0
+        
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                count += 1
+                res = max(res, count)
+            else:
+                count = 0
+
+        return res
